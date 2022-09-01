59b5f616cebe9b24d0835b2882538acd3b49e2ac
Matheus Silverio <matheusphillipo@gmail.com>
2022-09-01 01:34:12 -0300

    Addition of a changeColor number function

--- Day 1/script.js
+++ Day 1/script.js
@@ -7,16 +7,28 @@ const buttons = document.querySelector(".container-buttons")
 buttons.addEventListener("click", (event) => {
     if (event.target.classList.contains("add")){
         count.innerHTML++
+        setCounterColor()
     }
     else if (event.target.classList.contains("subtract")){
         count.innerHTML--
+        setCounterColor()
     }
     else if (event.target.classList.contains("reset")){
         count.innerHTML = 0
+        setCounterColor()
     }
 })
 
 
+function setCounterColor(){
+    if(count.innerHTML > 0){
+        count.style.color = "lightgreen"
+    }else if(count.innerHTML < 0){
+        count.style.color = "red"
+    }else{
+        count.style.color = "white"
+    }
+}
 
 
 
